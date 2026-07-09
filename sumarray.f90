bb08f71d93a1839907059d4190335ecfb8aaafaf
ikavvii <immkavin.ofcl@gmail.com>
2026-07-09 10:49:38 +0530

    Add array sum

---
+++ sumarray.f90
@@ -0,0 +1,29 @@
+program array_sum_input
+    implicit none
+
+    integer :: N, i
+    real, allocatable :: A(:)
+    real :: total_sum
+
+    print *, "Enter the number of elements in the array:"
+    read *, N
+
+    allocate(A(N))
+
+    print *, "Enter ", N, " real numbers:"
+    do i = 1, N
+        read *, A(i)
+    end do
+
+    total_sum = 0.0
+
+    ! Sequential sum
+    do i = 1, N
+        total_sum = total_sum + A(i)
+    end do
+
+    print *, "The sum of elements is: ", total_sum
+
+    deallocate(A)
+
+end program array_sum_input
